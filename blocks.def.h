9b8f2656f54a1185ff97c7e046e74383c306a3b6
Hemanth <hemanth.kotagiri43@gmail.com>
2021-10-14 20:59:32 +0530

    Add Volume and memory Scripts

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"🧠 ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"🔊 ", "volume",					1,		0},
+	{"", "date '+%A %d-%m-%y | ⌛%I:%M:%S %p'",					1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
